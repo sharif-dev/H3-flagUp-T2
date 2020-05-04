38f739df9786abc3b134fbac129503cd19ccb183
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-05-05 02:39:53 +0430

    Bug fix, code refactor

--- app/src/main/java/com/example/sensors/ShakeDetectionService.java
+++ app/src/main/java/com/example/sensors/ShakeDetectionService.java
@@ -1,61 +1,48 @@
 package com.example.sensors;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
-import android.view.Window;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-public class ShakeDetectionService extends Service {
+public class ShakeDetectionService extends Service implements SensorEventListener {
 
-    SensorManager sm;
+    SensorManager sensorManager;
     Sensor accelerometer;
 
     private float accVal;
     private float lastAccVal;
     private float shake;
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
 
-    boolean firstTime = true;
+        lastAccVal = accVal;
+        accVal = (float) Math.sqrt( (double) x*x + y*y + z*z );
+        float delta = accVal - lastAccVal;
+        shake = shake * 0.9f + delta;
 
-    private final SensorEventListener sensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            lastAccVal = accVal;
-            accVal = (float) Math.sqrt( (double) x*x + y*y + z*z );
-            float delta = accVal - lastAccVal;
-            shake = shake * 0.9f + delta;
-
-            if (shake > 6) {
+        if (shake > 6) {
 //                Toast toast = Toast.makeText(getApplicationContext(), "Do Not Shake Me", Toast.LENGTH_LONG);
 //                toast.show();
-                Log.i("shake activity", "Shake detection");
-                startActivityFunc();
-
-            }
+            Log.i("shake activity", "Shake detection");
+            startActivityFunc();
         }
+    }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
     private void startActivityFunc() {
 
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
@@ -74,14 +61,17 @@ public class ShakeDetectionService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sm.registerListener(sensorListener, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         accVal = SensorManager.GRAVITY_EARTH;
         lastAccVal = SensorManager.GRAVITY_EARTH;
         shake = 0.00f;
-
     }
 
-
-
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
+    }
 }
